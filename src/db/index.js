4ea78b2ef0ac7db93a00ddf4b1d29d7ccb4519bf
karun217 <karun.20224078@mnnit.ac.in>
2024-10-17 16:50:09 +0530

    utils code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: ${connection.connection.host}`);
+    console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.error("MONGODB connection failed: ", error);
     process.exit(1); // Exit the app on error
   }
 };
+
+
 
 export default connectDB;
