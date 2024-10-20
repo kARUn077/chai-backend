3ed37c4f4607b9022cbe299def9a27d0d39457f0
karun217 <karun.20224078@mnnit.ac.in>
2024-10-20 23:32:24 +0530

    upload file

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from 'multer'
+
+//we use disk storage here , not memory storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')  //destination is public -> temp
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage })
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadonCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        //upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath , {response_type" "auto})
+
+        //file has been uplaod successfully
+        console.log("file is uploaded on cloudinary" , response.url)
+        return response;
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath)
+        //remove locally saved file temporary as uplaod operation got failed
+
+        return null;
+    }
+}
+
+export {uploadonCloudinary}
