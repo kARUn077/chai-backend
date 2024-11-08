145128449ccbb14d0dbdcae3274dee9ff23248b4
karun217 <karun.20224078@mnnit.ac.in>
2024-11-09 01:24:52 +0530

    add routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import {Router} from "express"
-import { registerUser  ,logoutUser, loginUser , refreshAccessToken} from "../controllers/user.controller.js";
+import { registerUser  ,logoutUser, loginUser , refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { verifyJWT } from './../middlewares/auth.middleware';
 
 const router = Router()
 
@@ -26,5 +27,15 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT , getCurrentUser)
+router.route("/update-account").patch(verifyJWT , updateAccountDetails)
+router.route("/avatar").patch(verifyJWT , upload.single("avatar") , updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT , upload.single("/coverImage") , updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile)   //becoz of req.params
+
+router.route("/history").get(verifyJWT , getWatchHistory)
 
 export default router
